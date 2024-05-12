24484e7ef83baf4b10dd1c3973b779aa8e776eb5
Женя <dtxtrump@gmail.com>
2024-05-12 23:35:28 +0300

    master v3

--- tests/test_suite_form.py
+++ tests/test_suite_form.py
@@ -1,5 +1,6 @@
-from selene import browser,  be, have, by
+from selene import browser, be, have, by, command
 import os.path
+
 
 def test_fill_form():
     """
@@ -18,22 +19,23 @@ def test_fill_form():
     browser.element('.react-datepicker__year-select').click().element(by.text('1970')).click()
     browser.element('.react-datepicker__day--029').click()
     browser.element('#subjectsInput').should(be.blank).type('Arts').press_enter()
-    browser.element('[for="hobbies-checkbox-2"]').click()
+    browser.element('[for="hobbies-checkbox-2"]').perform(command.js.scroll_into_view).click()
     browser.element('#uploadPicture').send_keys(os.path.abspath('img1.jpg'))
     browser.element('#currentAddress').should(be.blank).type("City Msc Random street")
     browser.element('#state').click().element(by.text('NCR')).click()
     browser.element('#city').click().element(by.text('Delhi')).click()
-    browser.element('#submit').click()
+    browser.element('#submit').perform(command.js.scroll_into_view).click()
 
-    browser.element('.modal-header').should(have.text("Thanks for submitting the form"))
-    browser.element("tbody tr:nth-child(1) td:nth-child(2)").should(have.text('FirstNameInput LastNameInput'))
-    browser.element("tbody tr:nth-child(2) td:nth-child(2)").should(have.text('emailName@contoso.com'))
-    browser.element("tbody tr:nth-child(3) td:nth-child(2)").should(have.text('Female'))
-    browser.element("tbody tr:nth-child(4) td:nth-child(2)").should(have.text('0123456789'))
-    browser.element("tbody tr:nth-child(5) td:nth-child(2)").should(have.text('29 April,1970'))
-    browser.element("tbody tr:nth-child(6) td:nth-child(2)").should(have.text('Arts'))
-    browser.element("tbody tr:nth-child(7) td:nth-child(2)").should(have.text('Reading'))
-    browser.element("tbody tr:nth-child(8) td:nth-child(2)").should(have.text('img1.jpg'))
-    browser.element("tbody tr:nth-child(9) td:nth-child(2)").should(have.text('City Msc Random street'))
-    browser.element("tbody tr:nth-child(10) td:nth-child(2)").should(have.text('NCR Delhi'))
-
+    browser.element('.modal-content').element('table').browser.all('tr').all('td').even.should(
+        have.exact_texts(
+            'FirstNameInput LastNameInput',
+            'emailName@contoso.com',
+            'Female',
+            '0123456789',
+            '29 April,1970',
+            'Arts',
+            'Reading',
+            'img1.jpg',
+            'City Msc Random street',
+            'NCR Delhi'
+        ))
