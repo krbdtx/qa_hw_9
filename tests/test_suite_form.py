baa95ca352eba82d5cf1186d900a90a819bd5b23
Женя <dtxtrump@gmail.com>
2024-05-12 13:04:46 +0300

    mid v2

--- tests/test_suite_form.py
+++ tests/test_suite_form.py
@@ -8,21 +8,22 @@ def test_fill_form():
     Успешно заполнено
     """
     reg_page.open_win()
-
-    reg_page.fill_first_name('FirstNameInput')
-    reg_page.fill_last_name('LastNameInput')
-    reg_page.fill_email('emailName@contoso.com')
-    reg_page.fill_gender('Female')
-    reg_page.fill_user_number('0123456789')
-    reg_page.fill_birthdate('1970', 'May', '29')
-    reg_page.fill_subjects('Arts')
-    reg_page.fill_hobbies('Reading')
-    reg_page.fill_img('img1.jpg')
-    reg_page.fill_current_address('City Msc Random street')
-    reg_page.fill_state('NCR')
-    reg_page.fill_city('Delhi')
-    reg_page.submit_reg()
-
+    (
+        reg_page
+        .fill_first_name('FirstNameInput')
+        .fill_last_name('LastNameInput')
+        .fill_email('emailName@contoso.com')
+        .fill_gender('Female')
+        .fill_user_number('0123456789')
+        .fill_birthdate('1970', 'May', '29')
+        .fill_subjects('Arts')
+        .fill_hobbies('Reading')
+        .fill_img('img1.jpg')
+        .fill_current_address('City Msc Random street')
+        .fill_state('NCR')
+        .fill_city('Delhi')
+        .submit_reg()
+    )
     reg_page.should_reg_user(
         'FirstNameInput',
         'LastNameInput',
